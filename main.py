1c758514a89f7e139bc06d547fefd5cf1ff15cfb
sjamesjr <sam@barprints.com>
2025-02-17 05:49:42 +0100

    bug fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from movie_app import MovieApp
+from storage_json import StorageJson
 from storage_csv import StorageCSV
 
-storage = StorageCSV('movies.csv')
+storage = StorageJson('movies.json')
 movie_app = MovieApp(storage)
 movie_app.run()
